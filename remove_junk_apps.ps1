19c072526731045ad7926a5184619dfab360933b
leanhtai01 <leanhtai01@gmail.com>
2021-01-31 14:56:36 +0700

    add file remove_junk_apps.ps1

---
+++ remove_junk_apps.ps1
@@ -0,0 +1,22 @@
+$app_list = @(
+	"Microsoft.BingWeather",
+	"SpotifyAB.SpotifyMusic",
+	"Microsoft.Office.OneNote",
+	"Microsoft.MicrosoftOfficeHub",
+	"Microsoft.WindowsFeedbackHub",
+	"Microsoft.ZuneMusic",
+	"Microsoft.ZuneVideo",
+	"Microsoft.MixedReality.Portal",
+	"Microsoft.Microsoft3DViewer",
+	"Microsoft.YourPhone",
+	"Microsoft.People",
+	"Microsoft.WindowsSoundRecorder",
+	"Microsoft.WindowsMaps",
+	"Microsoft.MSPaint",
+	"Microsoft.SkypeApp",
+	"Microsoft.MicrosoftSolitaireCollection"
+)
+
+foreach ($app in $app_list) {
+	Get-AppxPackage -Name $app | Remove-AppxPackage
+}
