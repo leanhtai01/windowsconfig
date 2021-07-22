6850520f74ee6e0f74caf33a80ae38c07ead5317
leanhtai01 <leanhtai01@gmail.com>
2021-07-23 00:50:07 +0700

    add new .emacs file

--- configure.ps1
+++ configure.ps1
@@ -1,5 +1,5 @@
 ﻿## configure Emacs
-Copy-Item "./BAKWindows.emacs" -Destination "c:/Users/leanh/AppData/Roaming/.emacs"
+Copy-Item "./BAKWindows.emacs" -Destination "c:/Users/leanhtai01/AppData/Roaming/.emacs"
 
 ## configure AutoHotkey
 New-Item -Path "c:/" -Name "autohotkey_scripts" -ItemType "directory"
@@ -7,7 +7,7 @@ Copy-Item "./open_programs.ahk" -Destination "c:/autohotkey_scripts"
 
 # make AutoHotkey script run at startup
 $file_path = "c:/autohotkey_scripts/open_programs.ahk"
-$shortcut_path = "C:\Users\leanh\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\open_programs.ahk.lnk"
+$shortcut_path = "C:\Users\leanhtai01\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\open_programs.ahk.lnk"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $shortcut = $WScriptShell.CreateShortcut($shortcut_path)
 $shortcut.TargetPath = $file_path
